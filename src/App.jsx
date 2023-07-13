c31d38a66d00c34b3129fbe3be4eb948b2f999e5
Sujin Lee <soolovepat@naver.com>
2023-07-13 11:42:54 +0900

    Fix addTodo & deleteTodo

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
   Button,
   InputContainer,
@@ -7,23 +7,31 @@ import {
   TodoContainer,
   TodoHeader,
   TodoListContainer,
-} from './components/styles';
-import nextId from 'react-id-generator';
-import { useDispatch, useSelector } from 'react-redux';
-import { __addToDo, __deleteTodo } from './redux/modules/todosSlice';
+} from "./components/styles";
+import nextId from "react-id-generator";
+import { useDispatch, useSelector } from "react-redux";
+import { __addToDo, __deleteTodo } from "./redux/modules/todosSlice";
 
 function App() {
   const id = nextId();
   const dispatch = useDispatch();
   const todos = useSelector((state) => state.todos.list);
-  const [title, setTitle] = useState('');
-  const [body, setBody] = useState('');
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
 
   const onAddTodo = () => {
     /**
      * 시험 문제 1.
      * 이곳에서 추가하기 기능을 구현해주세요.
      */
+    const newTodo = {
+      id,
+      title,
+      body,
+    };
+    dispatch(__addToDo(newTodo));
+    //console.log(todos);
+    resetInputs();
   };
 
   const onDeleteTodo = (id) => {
@@ -31,14 +39,16 @@ function App() {
      * 시험 문제 2.
      * 이곳에서 삭제하기 기능을 구현해주세요.
      */
+    dispatch(__deleteTodo(id));
+    console.log(id);
   };
 
   const resetInputs = () => {
     /**
      * 입력 값을 초기화하고 싶다면 사용하세요.
      */
-    setTitle('');
-    setBody('');
+    setTitle("");
+    setBody("");
   };
   const onChangeTitle = (e) => setTitle(e.target.value);
   const onChangeBody = (e) => setBody(e.target.value);
@@ -60,14 +70,14 @@ function App() {
             onChange={onChangeBody}
           />
 
-          <Button>+ 추가하기</Button>
+          <Button onClick={onAddTodo}>+ 추가하기</Button>
         </InputContainer>
         <TodoListContainer>
           {todos.map((todo) => (
             <TodoCard key={todo.id}>
               <span>제목: {todo.title}</span>
               <span>할 일: {todo.body}</span>
-              <Button>삭제하기</Button>
+              <Button onClick={() => onDeleteTodo(todo.id)}>삭제하기</Button>
             </TodoCard>
           ))}
         </TodoListContainer>
--- src/redux/modules/todosSlice.js
+++ src/redux/modules/todosSlice.js
@@ -1,32 +1,53 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-
-/**
- * 2초 지연을 시키는 함수입니다 (비동기 작업).
- */
-import { waitTwoSeconds } from '../../utils';
-
-export const __addToDo = createAsyncThunk(
-  '__addToDo',
-  async (payload, thunkAPI) => {}
-);
-
-export const __deleteTodo = createAsyncThunk(
-  '__deleteToDo',
-  async (payload, thunkAPI) => {}
-);
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { waitTwoSeconds } from "../../utils";
 
 const initialState = {
   list: [],
+  isLoading: false,
 };
 
 const todosSlice = createSlice({
-  name: 'todos',
+  name: "todos",
   initialState,
   reducers: {
-    addTodo: (state, action) => {},
-    deleteTodo: (state, action) => {},
+    addTodo: (state, action) => {
+      const newTodo = action.payload;
+      state.list = [...state.list, newTodo];
+      // console.log(state.list);
+    },
+    deleteTodo: (state, action) => {
+      const id = action.payload;
+      state.list = state.list.filter((todo) => todo.id !== id);
+    },
+    todoLoading: (state) => {
+      state.isLoading = true;
+    },
   },
 });
+
+export const __addToDo = createAsyncThunk(
+  "__addToDo",
+  async (payload, thunkAPI) => {
+    thunkAPI.dispatch(todosSlice.actions.todoLoading());
+    await waitTwoSeconds(); // 2초 지연
+
+    thunkAPI.dispatch(todosSlice.actions.addTodo(payload));
+    // console.log(payload);
+    return payload;
+  }
+);
+
+export const __deleteTodo = createAsyncThunk(
+  "__deleteToDo",
+  async (payload, thunkAPI) => {
+    thunkAPI.dispatch(todosSlice.actions.todoLoading());
+    await waitTwoSeconds(); // 2초 지연
+
+    thunkAPI.dispatch(todosSlice.actions.deleteTodo(payload));
+    // console.log(payload);
+    return payload;
+  }
+);
 
 export const { addTodo, deleteTodo } = todosSlice.actions;
 export default todosSlice.reducer;
